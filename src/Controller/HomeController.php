48a348210ce4c8ec9f82c2e782a11f54da75e0fb
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-04-25 11:16:52 +0200

    création de pages pour présenter les cocktails

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,110 @@
+<?php
+// Défini le chemin de ce fichier
+// Obligatoire, doit représenter exactement le chemin du fichier en remplaçant le dossier "src" par "App"
+namespace App\Controller;
+
+// Remplace le require
+// On indique ici le namespace de la classe qu'on veut utiliser et Symfony + composer font le require automatiquement
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use \DateTime;
+
+class HomeController extends AbstractController
+{
+
+    #[Route('/', name:'home')]
+
+    public function home()
+    {
+        $cocktails = [
+            1 => [
+                'id'            => 1,
+                'nom'           => 'Mojito',
+                'image'         => 'https://i0.wp.com/cuisinovores.com/wp-content/uploads/2025/04/photo_cocktail_mojito_cuisinovores.webp?fit=500%2C500&ssl=1', // photo libre de droits
+                'ingredients'   => [
+                    '50 ml de rhum blanc',
+                    '½ citron vert (en quartiers)',
+                    '2 c.à.c. de sucre de canne',
+                    '8 feuilles de menthe fraîche',
+                    'Eau pétillante',
+                    'Glace pilée'
+                ],
+                'date_creation' => '1942-01-01',
+                'description'   => 'Classique cubain ultra-rafraîchissant mêlant menthe et citron vert.'
+            ],
+
+            2 => [
+                'id'            => 2,
+                'nom'           => 'Margarita',
+                'image'         => 'https://cocktailmolotov.org/wp-content/uploads/2012/04/Margarita-1.jpg',
+                'ingredients'   => [
+                    '50 ml de tequila',
+                    '25 ml de triple sec (Cointreau)',
+                    '25 ml de jus de citron vert frais',
+                    'Sel pour givrer le verre',
+                    'Glace'
+                ],
+                'date_creation' => '1938-07-04',
+                'description'   => 'Tequila, triple-sec et citron vert dans un verre givré de sel pour un équilibre acidulé-salé.'
+            ],
+
+            3 => [
+                'id'            => 3,
+                'nom'           => 'Old Fashioned',
+                'image'         => 'https://s3-eu-west-1.amazonaws.com/images-ca-1-0-1-eu/recipe_photos/original/209466/old-fashioned-cocktail-AdobeStock_222874229.jpg',
+                'ingredients'   => [
+                    '60 ml de bourbon ou rye whisky',
+                    '1 morceau de sucre',
+                    '2 traits d’angostura bitters',
+                    'Zeste d’orange',
+                    'Glaçon gros format'
+                ],
+                'date_creation' => '1880-05-15',
+                'description'   => 'Icône des classiques : un whisky subtilement sucré et aromatisé aux bitters.'
+            ],
+
+            4 => [
+                'id'            => 4,
+                'nom'           => 'Piña Colada',
+                'image'         => 'https://www.oldnick.fr/wp-content/uploads/2020/02/cocktail-pina-colada-scaled.jpg',
+                'ingredients'   => [
+                    '60 ml de rhum blanc',
+                    '90 ml de jus d’ananas',
+                    '30 ml de crème de coco',
+                    'Glaçons'
+                ],
+                'date_creation' => '1954-08-16',
+                'description'   => 'Spécialité portoricaine crémeuse et fruitée à base d’ananas et de coco.'
+            ],
+
+            5 => [
+                'id'            => 5,
+                'nom'           => 'Negroni',
+                'image'         => 'https://ginsiders.com/wp-content/uploads/2024/08/Ginsiders-recettes-1.jpg',
+                'ingredients'   => [
+                    '30 ml de gin',
+                    '30 ml de vermouth rouge',
+                    '30 ml de Campari',
+                    'Zeste d’orange',
+                    'Glaçon gros format'
+                ],
+                'date_creation' => '1919-06-01',
+                'description'   => 'Amertume élégante et notes d’agrumes pour ce grand classique italien.'
+            ],
+        ];
+
+        // Trie les cocktails par date de création dans un ordre décroissant
+        usort($cocktails, function($a, $b) {
+            // Crée des objets DateTime à partir des dates de création
+            $dateA = new DateTime($a['date_creation']);
+            $dateB = new DateTime($b['date_creation']);
+            return $dateB <=> $dateA; // Compare les dates et les retourne dans l'ordre décroissant (plus récent au plus vieux)
+        });
+
+        // Utilisation de la méthode render qui permet de récupérer un fichier de view twig
+        return $this->render('home.html.twig', [
+            // Découpe le tableau 'cocktails' et prend ses 2 derniers cocktails (dans l'ordre décroissant défini au dessus)
+            'cocktails' => array_slice($cocktails, 0, 2),
+        ]);
+    }
+}

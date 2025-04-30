53b489dfd447ad4a152d5341fda4e61839f71d90
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-04-30 14:37:32 +0200

    Ajout d'Entity pour créer un cocktail

--- src/Entity/Cocktail.php
+++ src/Entity/Cocktail.php
@@ -20,13 +20,16 @@ class Cocktail {
 
     public $isPublished;
 
+    // méthode pour créer un cocktail
     public function __construct($name, $ingredients, $description, $image) {
 
+        // valeurs envoyées par l'utilisateur
         $this->name = $name;
         $this->ingredients = $ingredients;
         $this->description = $description;
         $this->image = $image;
 
+        // valeurs remplis automatiquement lors de l'envoi des données par l'utilisateur
         $this->createdAt = new DateTime();
         $this->isPublished = true;
         $this->id = 6;
